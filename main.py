b47f307ba5c0f32b91672645406a0b01a4a55f22
Ignasi <icrespi@institutmvm.cat>
2021-01-21 13:21:42 +0100

    inicial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+
+
+def main():
+    not1 = input("Afegeix la primera nota")
+        if not1<5
+            []
+    not2 = input("Afegeix la primera nota")
+    not3 = input("Afegeix la primera nota")
+    not4 = input("Afegeix la primera nota")
+    not5 = input("Afegeix la primera nota")
+    not6 = input("Afegeix la primera nota")
+    not7 = input("Afegeix la primera nota")
+    not8 = input("Afegeix la primera nota")
+    not9 = input("Afegeix la primera nota")
+    not10 = input("Afegeix la primera nota")
+    not11 = input("Afegeix la primera nota")
+    not12 = input("Afegeix la primera nota")
+    not13 = input("Afegeix la primera nota")
+    not14 = input("Afegeix la primera nota")
+    not15 = input("Afegeix la primera nota")
+
+    print("", not3)
+
+if __name__ == "__main__":
+    main()
